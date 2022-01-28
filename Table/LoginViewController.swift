0f0e4a9d7851f42f659b966b419fba5d2bbce61d
SergeyYkovlev <yakovlevserg09@gmail.com>
2022-01-28 17:27:14 +0600

    refactoring

--- Table/LoginViewController.swift
+++ Table/LoginViewController.swift
@@ -10,151 +10,165 @@ import UIKit
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
     
+    var iconClick : Bool!
+    var changed: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        showPasswordButton.setImage((UIImage(named: "noshow")), for: .normal)
+        
+        view.backgroundColor = .white
+        firsNameTextField.backgroundColor = .systemGray
+        lastNameTextField.backgroundColor = .systemGray
+        emailTextField.backgroundColor = .systemGray
+        passwordTextField.backgroundColor = .systemGray
+        clearLastNameButton.isHidden = true
+        clearFirstNameButton.isHidden = true
+        clearEmailButton.isHidden = true
+        clearPasswordButton.isHidden = true
+        
+        firsNameTextField.addTarget(self, action: #selector(firstNameTextFieldDidChange), for: .editingChanged)
+        lastNameTextField.addTarget(self, action: #selector(lastNameTextFieldDidChange), for: .editingChanged)
+        emailTextField.addTarget(self, action: #selector(emailTextFieldDidChange), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(passwordTextFieldDidChange), for: .editingChanged)
+        iconClick = true
+    }
+    
+    // MARK: - Subviews
+    
     @IBOutlet weak var firsNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var showPasswordButton: UIButton!
+    @IBOutlet weak var clearFirstNameButton: UIButton!
+    @IBOutlet weak var clearPasswordButton: UIButton!
+    @IBOutlet weak var clearEmailButton: UIButton!
+    @IBOutlet weak var clearLastNameButton: UIButton!
+    @IBOutlet weak var warningEmailLabel: UILabel!
+    @IBOutlet weak var warningLastNameLabel: UILabel!
+    @IBOutlet weak var warningFirstNameLabel: UILabel!
+    @IBOutlet weak var warningPasswordLabel: UILabel!
     
-    
-    
-    @IBOutlet weak var showFirstName: UIButton!
-    
-    @IBOutlet weak var showPassword: UIButton!
-    @IBOutlet weak var showEmail: UIButton!
-    @IBOutlet weak var showLastName: UIButton!
-    @IBOutlet weak var resultEmail: UILabel!
-    @IBOutlet weak var resultLastName: UILabel!
-    @IBOutlet weak var resultFirstName: UILabel!
-    @IBOutlet weak var resultPassword: UILabel!
-    
-    var iconClick : Bool!
-    var changed: Bool = false
-    
-   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        showLastName.isHidden = true
-        showFirstName.isHidden = true
-        showEmail.isHidden = true
-        showPassword.isHidden = true
-        
-        firsNameTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
-        lastNameTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
-        emailTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
-        passwordTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
-        iconClick = true
-    }
-    
-//    func touch() {
-//        if firsNameTextField.text == "" {
-//            showClearfirstName.isHidden = true
-//        }else {
-//            showClearfirstName.isHidden = false
-//        }
-//
-//    }
-    
-    
-    @objc func textFieldDidChange(textField: UITextField) {
+    @objc func firstNameTextFieldDidChange(textField: UITextField) {
         print(textField.text)
         
-        showFirstName.isHidden = false
+        clearFirstNameButton.isHidden = false
+        firsNameTextField.backgroundColor = .systemGray
+        warningFirstNameLabel.text = ""
         if firsNameTextField.text == "" {
-            showFirstName.isHidden = true
+            clearFirstNameButton.isHidden = true
+            firsNameTextField.backgroundColor = .systemGray
+            warningFirstNameLabel.text = ""
         }
-        showLastName.isHidden = false
+    }
+    @objc func lastNameTextFieldDidChange(textField: UITextField) {
+        clearLastNameButton.isHidden = false
+        lastNameTextField.backgroundColor = .systemGray
+        warningLastNameLabel.text = ""
         if lastNameTextField.text == "" {
-            showLastName.isHidden = true
-        }
-        showEmail.isHidden = false
-        if emailTextField.text == "" {
-            showEmail.isHidden = true
-        }
-        showPassword.isHidden = false
-        if passwordTextField.text == "" {
-            showPassword.isHidden = true
+            clearLastNameButton.isHidden = true
+            lastNameTextField.backgroundColor = .systemGray
+            //resultLastName.text = ""
         }
     }
     
+    @objc func emailTextFieldDidChange(textField: UITextField) {
+        clearEmailButton.isHidden = false
+        emailTextField.backgroundColor = .systemGray
+        warningEmailLabel.text = ""
+        if emailTextField.text == "" {
+            clearEmailButton.isHidden = true
+            emailTextField.backgroundColor = .systemGray
+            // resultEmail.text = ""
+        }
+    }
     
+    @objc func passwordTextFieldDidChange(textField: UITextField) {
+        clearPasswordButton.isHidden = false
+        passwordTextField.backgroundColor = .systemGray
+        warningPasswordLabel.text = ""
+        if passwordTextField.text == "" {
+            clearPasswordButton.isHidden = true
+            passwordTextField.backgroundColor = .systemGray
+            //resultPassword.text = ""
+        }
+    }
+    
+    // MARK: - Actions
     
     @IBAction func show(_ sender: Any) {
         if ( iconClick == true) {
             passwordTextField.isSecureTextEntry = false
+            showPasswordButton.setImage((UIImage(named: "show")), for: .normal)
             iconClick = false
         } else {
             passwordTextField.isSecureTextEntry = true
+            showPasswordButton.setImage((UIImage(named: "noshow")), for: .normal)
             iconClick = true
         }
-        
     }
     
-    
-    
-    
-
     func firstName() {
         if firsNameTextField.text?.isEmpty ?? true {
-            resultFirstName.text = "это поле не может быть пустым"
-            resultFirstName.textColor = .systemPink
-            resultFirstName.isHidden = false
+            warningFirstNameLabel.text = "это поле не может быть пустым"
+            warningFirstNameLabel.textColor = .systemPink
+            warningFirstNameLabel.isHidden = false
             firsNameTextField.backgroundColor = UIColor.systemPink
         } else {
             //сделать пустым
-            resultFirstName.isHidden = true
+            warningFirstNameLabel.isHidden = true
             firsNameTextField.backgroundColor = UIColor.systemGreen
-            
         }
     }
     
     func lastName() {
         if lastNameTextField.text?.isEmpty ?? true {
-            resultLastName.text = "это поле не может быть пустым"
-            resultLastName.textColor = .systemPink
-            resultLastName.isHidden = false
+            warningLastNameLabel.text = "это поле не может быть пустым"
+            warningLastNameLabel.textColor = .systemPink
+            warningLastNameLabel.isHidden = false
             lastNameTextField.backgroundColor = UIColor.systemPink
         } else {
-            resultLastName.isHidden = true
+            warningLastNameLabel.isHidden = true
             lastNameTextField.backgroundColor = UIColor.systemGreen
         }
     }
     
     func email(str: String) {
         if emailTextField.text?.isEmpty ??  true {
-            resultEmail.text = "это поле не может быть пустым"
-            resultEmail.textColor = .systemPink
-            resultEmail.isHidden = false
+            warningEmailLabel.text = "это поле не может быть пустым"
+            warningEmailLabel.textColor = .systemPink
+            warningEmailLabel.isHidden = false
             emailTextField.backgroundColor = UIColor.systemPink
         } else if !isValidEmail(str: str)  {
-            resultEmail.text = "Введите правильный email"
-            resultEmail.textColor = .systemPink
-            resultEmail.isHidden = false
+            warningEmailLabel.text = "Введите правильный email"
+            warningEmailLabel.textColor = .systemPink
+            warningEmailLabel.isHidden = false
             emailTextField.backgroundColor = UIColor.systemPink
         } else {
-            resultEmail.isHidden = true
+            warningEmailLabel.isHidden = true
             emailTextField.backgroundColor = UIColor.systemGreen
         }
     }
     
     func password() {
         if passwordTextField.text?.isEmpty ?? true  {
-            resultPassword.text = "это поле не может быть пустым"
-            resultPassword.textColor = .systemPink
-            resultPassword.isHidden = false
+            warningPasswordLabel.text = "это поле не может быть пустым"
+            warningPasswordLabel.textColor = .systemPink
+            warningPasswordLabel.isHidden = false
             passwordTextField.backgroundColor = UIColor.systemPink
         } else if passwordTextField.text!.count < 6 {
-            resultPassword.text = "Пароль не может быть короче 6 символов"
-            resultPassword.textColor = .systemPink
-            resultPassword.isHidden = false
+            warningPasswordLabel.text = "Пароль не может быть короче 6 символов"
+            warningPasswordLabel.textColor = .systemPink
+            warningPasswordLabel.isHidden = false
             passwordTextField.backgroundColor = UIColor.systemPink
             
         } else {
-            resultPassword.isHidden = true
+            warningPasswordLabel.isHidden = true
             passwordTextField.backgroundColor = UIColor.systemGreen
         }
     }
-    
     
     func isValidEmail(str: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
@@ -164,31 +178,30 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func clearFirstName(_ sender: Any) {
         firsNameTextField.text = ""
-        firsNameTextField.backgroundColor = .white
-        resultFirstName.text = ""
-        showFirstName.isHidden = true
+        firsNameTextField.backgroundColor = .systemGray
+        warningFirstNameLabel.text = ""
+        clearFirstNameButton.isHidden = true
     }
     
     @IBAction func clearLastName(_ sender: Any) {
         lastNameTextField.text = ""
-        lastNameTextField.backgroundColor = .white
-        resultLastName.text = ""
-        showLastName.isHidden = true
-        
+        lastNameTextField.backgroundColor = .systemGray
+        warningLastNameLabel.text = ""
+        clearLastNameButton.isHidden = true
     }
     
     @IBAction func clearPassword(_ sender: Any) {
         passwordTextField.text = ""
-        passwordTextField.backgroundColor = .white
-        resultPassword.text = ""
-        showPassword.isHidden = true
+        passwordTextField.backgroundColor = .systemGray
+        warningPasswordLabel.text = ""
+        clearPasswordButton.isHidden = true
     }
     
     @IBAction func clearEmail(_ sender: Any) {
         emailTextField.text = ""
-        emailTextField.backgroundColor = .white
-        resultEmail.text = ""
-        showEmail.isHidden = true
+        emailTextField.backgroundColor = .systemGray
+        warningEmailLabel.text = ""
+        clearEmailButton.isHidden = true
     }
     
     @IBAction func send(_ sender: Any) {
@@ -199,8 +212,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         }
         
         password()
-        
-        
+       
     }
     
 }
